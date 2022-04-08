f14bba6a2be0ad083e687842fce8ee78b31f3e3d
UnicFb910 <unaifb910@gmail.com>
2022-04-08 16:57:19 +0200

    Minor fix

---
+++ build/precache-manifest.902c70e7efa02375e772f66b380c0a16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0836d89a0dea07118dd483e876bbbb46",
+    "url": "/my-wave-portal/index.html"
+  },
+  {
+    "revision": "fbc89428a0a816a5dfaf",
+    "url": "/my-wave-portal/static/css/main.233cbc38.chunk.css"
+  },
+  {
+    "revision": "c23d5974e2945618490f",
+    "url": "/my-wave-portal/static/js/2.89a5c1db.chunk.js"
+  },
+  {
+    "revision": "fbc89428a0a816a5dfaf",
+    "url": "/my-wave-portal/static/js/main.2d8deef3.chunk.js"
+  },
+  {
+    "revision": "dc7b73033a3979fd32c3",
+    "url": "/my-wave-portal/static/js/runtime-main.bd638210.js"
+  }
+]);
